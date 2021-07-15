250d972d5105c495bf9929883d6f8a6b1900f495
dotnotation <dotinmotionproductions@gmail.com>
2021-07-15 18:51:10 -0400

    commit tree 37bde688135e08530cbcacd880d82fab16630a4e

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+    #this is our join table as artists have songs and songs have genres which connects genres to artist
+    def change
+        create_table :songs do |t|
+          t.string :name
+          t.integer :artist_id
+          t.integer :genre_id
+        end
+    end
 end
